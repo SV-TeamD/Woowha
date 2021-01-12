a162e5bc3ae2aa3497f3c9712572b42cd96c530b
Jiwon Jeong <poiu8944@gmail.com>
2021-01-12 15:47:27 +0900

    chore: load models at start

--- modelserver/start-dev.sh
+++ modelserver/start-dev.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+LWHITE="\033[1;37m"
+LCYAN="\033[1;36m"
+
+echo "${LCYAN}Model Server Start"
 # python3 manage.py run
+echo ""
+echo "${LWHITE}Download Pretrained Model Weights"
+bash ./load-models.sh
+echo ""
+echo "${LWHITE}TEST"
+pytest
+echo ""
+echo "${LWHITE}Create image from GAN"
+echo "Pytorch Models CartoonGAN [Chen et al., CVPR18]. With the released pretrained models by the authors."
 python3 network/Runner.py
