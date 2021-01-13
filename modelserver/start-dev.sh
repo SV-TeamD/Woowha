6f1d94a90b30b1efc77b1def958635aeb9bc58e5
Jiwon Jeong <poiu8944@gmail.com>
2021-01-13 22:19:26 +0900

    feat: add RabbitMQ example

--- modelserver/start-dev.sh
+++ modelserver/start-dev.sh
@@ -4,14 +4,21 @@ LWHITE="\033[1;37m"
 LCYAN="\033[1;36m"
 
 echo "${LCYAN}Model Server Start"
-# python3 manage.py run
+
 echo ""
 echo "${LWHITE}Download Pretrained Model Weights"
 bash ./load-models.sh
 echo ""
-echo "${LWHITE}TEST"
+echo "${LWHITE}RUN TEST"
 pytest
 echo ""
 echo "${LWHITE}Create image from GAN"
 echo "Pytorch Models CartoonGAN [Chen et al., CVPR18]. With the released pretrained models by the authors."
-python3 network/Runner.py
+
+## wait-for-it.sh ##
+# Usage:
+#     ...
+#     -- COMMAND ARGS             Execute command with args after the test finishes
+bash ../wait-for-it.sh rabbitmq:15672 -s -t 30
+python3 app/worker.py
+# python3 network/Runner.py
--- webserver/start-dev.sh
+++ webserver/start-dev.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-python3 src/app.py
+LWHITE="\033[1;37m"
+LCYAN="\033[1;36m"
+
+echo "${LCYAN}Web Server Start"
+
+## wait-for-it.sh ##
+# Usage:
+#     ...
+#     -- COMMAND ARGS             Execute command with args after the test finishes
+bash ../wait-for-it.sh rabbitmq:15672 -s -t 30
+python3 src/new_task.py
