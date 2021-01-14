d18a22138ccf9bdbaf467678c0e6e61635ef3032
Jiwon Jeong <poiu8944@gmail.com>
2021-01-15 01:06:37 +0900

    feat: image route
    /image/upload
    /image/result/filename?author=Hayao
    
    - /image/upload: file save in local and add job in Message Queue

---
+++ webserver/src/JobProducer.py
@@ -0,0 +1,23 @@
+import pika
+
+
+parameters = pika.ConnectionParameters("rabbitmq")
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+
+class JobProducer:
+    def __init__(self):
+        channel.queue_declare(queue="job_queue", durable=True)
+
+    def add_job(self, message):
+        channel.basic_publish(
+            exchange="",
+            routing_key="job",
+            body=message,
+            properties=pika.BasicProperties(delivery_mode=2),
+        )
+        print("Sent {}".format(message))
+
+    def close_connection(self):
+        connection.close()
--- webserver/src/app.py
+++ webserver/src/app.py
@@ -1,14 +1,16 @@
-import time
-
-from flask import Flask, jsonify
-
-
-app = Flask(__name__)
+from flask import Flask
+from routes import main_route, image_route
 
 
-@app.route("/", methods=["GET"])
-def hello():
-    return "Flask in a Docker!!! Hello World!"
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(main_route.bp)
+    app.register_blueprint(image_route.bp)
+
+    return app
 
 
-app.run(host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
