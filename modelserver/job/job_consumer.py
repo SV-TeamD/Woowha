1e04181540ddd4859137a87f81d4c862d859ade5
Jiwon Jeong <poiu8944@gmail.com>
2021-01-18 14:50:44 +0900

    refactor: lint modelserver

--- modelserver/job/job_consumer.py
+++ modelserver/job/job_consumer.py
@@ -16,24 +16,28 @@ runner = Runner(input_dir=INPUT_FOLDER, output_dir=OUTPUT_FOLDER)
 
 # TODO: MessageQueue abstract 만들고 상속하자
 class JobConsumer:
-    def __init__(self):
+    @classmethod
+    def __init__(cls):
         channel.queue_declare(queue="job_queue", durable=True)
         channel.basic_qos(prefetch_count=1)
         print("hi Job Consumer")
 
-    def generate_image(self, ch, method, properties, body):
+    @classmethod
+    def generate_image(cls, _channel, method, _, body):
         message = json.loads(body)
         print(" [x] Received {}".format(body.decode()))
         # TODO: generate image
         runner.run(imagefile_name=message["filename"], style=message["author"])
         print(" [x] Done")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        _channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    def start(self):
-        channel.basic_consume(queue="job_queue", on_message_callback=self.generate_image)
+    @classmethod
+    def start(cls):
+        channel.basic_consume(queue="job_queue", on_message_callback=cls.generate_image)
         channel.start_consuming()
 
-    def close_connection(self):
+    @classmethod
+    def close_connection(cls):
         connection.close()
 
 
--- modelserver/tests/model_test.py
+++ modelserver/tests/model_test.py
@@ -1,5 +1,2 @@
-from network import Runner
-
-
 def test_sum():
     assert (1 + 2 + 3) == 6
