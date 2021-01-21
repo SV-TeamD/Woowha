33347418d1decbbf534ad7bce397693baa1fa658
ByeongdoChoi <cookie023@naver.com>
2021-01-21 11:00:43 +0900

    feat: change form Structure

--- client/src/mainpage.js
+++ client/src/mainpage.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./mainpage.css";
 import img from "./img/empty_image.PNG";
+import hosoda_example from "./img/hosoda_example.PNG";
+import hayao_example from "./img/hayao_example.PNG";
+import paprika_example from "./img/paprika_example.PNG";
+import shinkai_example from "./img/shinkai_example.PNG";
 class mainpage extends Component {
   constructor(props) {
     super(props);
@@ -26,12 +30,17 @@ class mainpage extends Component {
     const st_img_style = {
       width: "220px",
       height: "267px",
-      marginRight: "40px",
     };
     const ov_img_style = {
       width: "80px",
       heigh: "80px",
       marginRight: "10px",
+    };
+    const author_name = {
+      width: "220px",
+      marginRight: "40px",
+      fontWeight: "bold",
+      fontSize: "20px",
     };
 
     let { imagePreviewUrl } = this.state;
@@ -44,7 +53,7 @@ class mainpage extends Component {
       <div>
         <br />
         <br />
-        <header>
+        <header className="head">
           <ul>
             <li>
               <a classNamde="active" href="#home">
@@ -65,37 +74,88 @@ class mainpage extends Component {
         <div className="upload">
           <h2>Image Upload</h2>
           <br />
+          <div className="imgPreview">{$imagePreview}</div>
           <br />
-          <div>
-            <div className="imgPreview">{$imagePreview}</div>
+          <br />
+          <form
+            id="upload_form"
+            action="http://locahost:5000/image/upload"
+            method="post"
+            encType="multipart/form-data"
+            target=""
+          >
+            <label id="file">
+              Upload
+              <input type="file" onChange={(e) => this._handleImageChange(e)} />
+            </label>
             <br />
             <br />
-            <form action="image_route.py" method="post">
-              <label id="upload_file">
-                <input
-                  type="file"
-                  onChange={(e) => this._handleImageChange(e)}
+            <br />
+            <div className="select">
+              <br />
+              <br />
+              <div className="author1">
+                <img
+                  src={hayao_example}
+                  alt="empty_image"
+                  style={st_img_style}
                 />
-                Upload
-              </label>
-            </form>
-          </div>
-          <br />
-          <br />
-          <br />
-          <br />
+                <h3>Hayao</h3>
+                <p>Explain</p>
+                <label id="author">
+                  <input type="radio" name="author" />
+                </label>
+              </div>
+
+              <div className="author2">
+                <img
+                  src={shinkai_example}
+                  alt="empty_image"
+                  style={st_img_style}
+                />
+                <h3>Shinkai</h3>
+                <p>Explain</p>
+                <label id="author">
+                  <input type="radio" name="author" />
+                </label>
+              </div>
+
+              <div className="author3">
+                <img
+                  src={paprika_example}
+                  alt="empty_image"
+                  style={st_img_style}
+                />
+                <h3>paprika</h3>
+                <p>Explain</p>
+                <label id="author">
+                  <input type="radio" name="author" />
+                </label>
+              </div>
+
+              <div className="author4">
+                <img
+                  src={hosoda_example}
+                  alt="empty_image"
+                  style={st_img_style}
+                />
+                <h3>Hosoda</h3>
+                <p>Explain</p>
+                <label id="author">
+                  <input type="radio" name="author" />
+                </label>
+              </div>
+
+              <br />
+              <br />
+              <br />
+              <button type="submit">Convert</button>
+            </div>
+          </form>
         </div>
-        <div className="select">
-          <br />
-          <br />
-          <img src={img} alt="empty_image" style={st_img_style} />
-          <img src={img} alt="empty_image" style={st_img_style} />
-          <img src={img} alt="empty_image" style={st_img_style} />
-          <img src={img} alt="empty_image" style={st_img_style} />
-          <br />
-          <br />
-          <button>Convert</button>
-        </div>
+        <br />
+        <br />
+
         <div className="overview">
           <h2>Overveiw</h2>
           <br />
