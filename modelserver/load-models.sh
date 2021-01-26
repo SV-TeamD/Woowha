49f6499a50caf6a4d98ff0031ed35d0a50733f87
iSuddenly <issuddenly@gmail.com>
2021-01-27 01:33:18 +0900

    feat: add links to download pretrained models

--- modelserver/load-models.sh
+++ modelserver/load-models.sh
@@ -3,8 +3,8 @@ BASE_PWD=$PWD
 
 # download pretrained models
 PRETRAINED_MODEL_PATH="./network/pretrained_model/"
-STYLES=("Hayao" "Hosoda" "Paprika" "Shinkai")
-MODEL_POSTFIX="_net_G_float.pth"
+STYLES=("cartoongan_hayao" "cartoongan_hosoda" "cartoongan_paprika" "cartoongan_shinkai" "cyclegan_cezanne" "cyclegan_monet" "cyclegan_ukiyoe" "cyclegan_vangogh")
+MODEL_POSTFIX=".pth"
 
 len=${#STYLES[@]}
 cd $PRETRAINED_MODEL_PATH
@@ -13,11 +13,16 @@ for ((i=0;i<len;i++)); do
   if [ -e "$FILEPATH" ]; then
     echo "$FILEPATH exist"
   else
-    url="http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/${STYLES[i]}_net_G_float.pth"
+    pthname = "${STYLES[i]}.pth"
+    url = "https://drive.google.com/drive/u/1/folders/126Oat85ofwr4ghIfVwNc7N_biFA3ehdv"
+    #url="http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/${STYLES[i]}_net_G_float.pth"
     echo "download ${STYLES[i]}"
-    wget -c $url
+    #wget -c $url
+    wget --no-check-certificate $url -O pthname
     echo
   fi
 done
+
+# wget --no-check-certificate 'https://drive.google.com/drive/u/1/folders/126Oat85ofwr4ghIfVwNc7N_biFA3ehdv' -O cyclegan_cezanne_300.pth
 
 cd $BASE_PWD
