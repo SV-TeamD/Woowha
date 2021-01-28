560c6c1ed08e9a93142f9121e6f9ef00917d7001
iSuddenly <issuddenly@gmail.com>
2021-01-28 17:16:03 +0900

    fix: fix load-models bug

--- modelserver/load-models.sh
+++ modelserver/load-models.sh
@@ -3,7 +3,6 @@ BASE_PWD=$PWD
 
 # download pretrained models
 PRETRAINED_MODEL_PATH="./network/pretrained_model/"
-<<<<<<< HEAD
 STYLES=("cartoongan_hayao.pth" "cartoongan_hosoda.pth" "cartoongan_paprika.pth" "cartoongan_shinkai.pth"
         "cyclegan_cezanne.pth" "cyclegan_monet.pth" "cyclegan_ukiyoe.pth" "cyclegan_vangogh.pth"
         "cartoongan_vangogh.ckpt" "cartoongan2_mulan.ckpt" "cartoongan2_pelissero.ckpt")
@@ -12,14 +11,13 @@ STYLES=("cartoongan_hayao.pth" "cartoongan_hosoda.pth" "cartoongan_paprika.pth" 
 len=${#STYLES[@]}
 cd $PRETRAINED_MODEL_PATH
 for ((i=0;i<len;i++)); do
-  FILEPATH="${STYLES[$i]}${MODEL_POSTFIX}"
+  FILEPATH="${STYLES[$i]}"
   if [ -e "$FILEPATH" ]; then
     echo "$FILEPATH exist"
   else
-    pthname = "${STYLES[i]}.pth"
-    url = "https://drive.google.com/drive/u/1/folders/126Oat85ofwr4ghIfVwNc7N_biFA3ehdv"
+    url="https://drive.google.com/drive/u/1/folders/126Oat85ofwr4ghIfVwNc7N_biFA3ehdv"
     echo "download ${STYLES[i]}"
-    wget $url -O pthname
+    wget $url -O ${STYLES[i]}
     echo
   fi
 done
