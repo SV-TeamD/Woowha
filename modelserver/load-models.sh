b4e4a3b95a04361e4c444981e596e6e44a364b52
iSuddenly <issuddenly@gmail.com>
2021-01-28 15:54:18 +0900

    feat: add new pretrained models

--- modelserver/load-models.sh
+++ modelserver/load-models.sh
@@ -3,8 +3,10 @@ BASE_PWD=$PWD
 
 # download pretrained models
 PRETRAINED_MODEL_PATH="./network/pretrained_model/"
-STYLES=("cartoongan_hayao" "cartoongan_hosoda" "cartoongan_paprika" "cartoongan_shinkai" "cyclegan_cezanne" "cyclegan_monet" "cyclegan_ukiyoe" "cyclegan_vangogh")
-MODEL_POSTFIX=".pth"
+STYLES=("cartoongan_hayao.pth" "cartoongan_hosoda.pth" "cartoongan_paprika.pth" "cartoongan_shinkai.pth"
+        "cyclegan_cezanne.pth" "cyclegan_monet.pth" "cyclegan_ukiyoe.pth" "cyclegan_vangogh.pth"
+        "cartoongan_vangogh.ckpt" "cartoongan2_mulan.ckpt" "cartoongan2_pelissero.ckpt")
+# MODEL_POSTFIX=".pth"
 
 len=${#STYLES[@]}
 cd $PRETRAINED_MODEL_PATH
@@ -15,10 +17,8 @@ for ((i=0;i<len;i++)); do
   else
     pthname = "${STYLES[i]}.pth"
     url = "https://drive.google.com/drive/u/1/folders/126Oat85ofwr4ghIfVwNc7N_biFA3ehdv"
-    #url="http://vllab1.ucmerced.edu/~yli62/CartoonGAN/pytorch_pth/${STYLES[i]}_net_G_float.pth"
     echo "download ${STYLES[i]}"
-    #wget -c $url
-    wget --no-check-certificate $url -O pthname
+    wget $url -O pthname
     echo
   fi
 done
